de0773b77411983adf98fff1973df10cd958eac6
HeyMahdy <156871399+HeyMahdy@users.noreply.github.com>
2024-01-18 06:58:18 +0000

    added some more code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , jsonify
 
 app = Flask(__name__)
 JOBS = [
@@ -14,11 +14,19 @@ JOBS = [
      'id' : 3,
      'title' : 'Admin',
      }
+  ,
+  {
+    'id' : 4,
+    'title' : 'Event Organizer',
+  
+  }
 ]
 @app.route('/')
 def Hello_world():
-    return render_template('home.html')
-
+    return render_template('home.html',jobs=JOBS)
+@app.route('/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 print(__name__)
 if (__name__== "__main__") :
     app.run(host='0.0.0.0', debug=True)
